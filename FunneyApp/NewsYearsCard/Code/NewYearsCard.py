bf1e094dd2f5bfdfda6038f750f67593ee44e30e
keviness <kevin1737@163.com>
2025-04-24 18:04:20 +0800

    新增简易计算器实现，包含基本运算功能和图形界面

---
+++ FunneyApp/NewsYearsCard/Code/NewYearsCard.py
@@ -0,0 +1,9 @@
+from pytools import pytools
+
+# This code is used to generate a new year card using the pytools library
+#tool_client_coupletgenerator = pytools.pytools()
+#tool_client_coupletgenerator.execute('coupletgenerator')
+
+
+tool_client_newyearcardgenerator = pytools.pytools()
+tool_client_newyearcardgenerator.execute('newyearcardgenerator')
